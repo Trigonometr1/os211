f432bd88adf7fd37b16dd693f803506b587eaf72
Taufiq Hadi Pratama <taufiqhadipratama@gmail.com>
2021-03-23 13:37:41 +0700

    Updated myscript and finished W03-fuse

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -1,32 +1,60 @@
 #!/bin/bash
-# Cicak bin Kadal
+# Copyright (C) 2020-2021 Cicak Bin Kadal
+# https://www.youtube.com/watch?v=KAXK07ni9gU
+
+# This free document is distributed in the hope that it will be 
+# useful, but WITHOUT ANY WARRANTY; without even the implied 
+# warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+# REV04 Mon 15 Mar 19:27:52 WIB 2021
+# REV03 Sun 14 Mar 18:21:27 WIB 2021
+# REV02 Fri 12 Mar 13:40:58 WIB 2021
+# REV01 Tue 13 Oct 10:37:14 WIB 2020
+# START Mon 28 Sep 21:05:04 WIB 2020
+
+# ATTN:
+# You new to set "REC2" with your own Public-Key Identity!
+# Check it out with "gpg --list-key" (Done)
 # Modified by Trigonometr1
 
-
-FILES="my*.txt my*.sh"
+REC2="taufiqhadipratama@gmail.com"
+REC1="operatingsystems@vlsm.org"
+FILES="my*.asc my*.txt my*.sh"
 SHA="SHA256SUM"
 
-# HApus file SHA256SUM yang lama
+[ -d $HOME/RESULT ] || mkdir -p $HOME/RESULT
+pushd $HOME/RESULT
+for II in W?? ; do
+    [ -d $II ] || continue
+    TARFILE=my$II.tar.bz2
+    TARFASC=$TARFILE.asc
+    rm -f $TARFILE $TARFASC
+    echo "tar cfj $TARFILE $II/"
+    tar cfj $TARFILE $II/
+    echo "gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE"
+    gpg --armor --output $TARFASC --encrypt --recipient $REC1 --recipient $REC2 $TARFILE
+done
+popd
+
+rm -f $HOME/RESULT/fakeDODOL
+for II in $HOME/RESULT/myW*.tar.bz2.asc $HOME/RESULT/fakeDODOL ; do
+   echo "Check and move $II..."
+   [ -f $II ] && mv -f $II .
+done
+
 echo "rm -f $SHA $SHA.asc"
 rm -f $SHA $SHA.asc
 
-# Buat SHA256SUM berdasarkan file dalam TXT/
 echo "sha256sum $FILES > $SHA"
 sha256sum $FILES > $SHA
 
-# Lakukan _checksum_
 echo "sha256sum -c $SHA"
 sha256sum -c $SHA
 
-# Buat _signature_ kita ke SHA256SUM.asc
 echo "gpg -o $SHA.asc -a -sb $SHA"
 gpg -o $SHA.asc -a -sb $SHA
 
-# Verifikasi _signature_ dengan gpg
 echo "gpg --verify $SHA.asc $SHA"
 gpg --verify $SHA.asc $SHA
 
 exit 0
-
-# Tue 29 Sep 2020 11:02:39 AM WIB
-# Thu 11 Mar 2021 12:13:28 AM WIB
